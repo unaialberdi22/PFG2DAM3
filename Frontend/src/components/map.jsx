ebc6cc12ce9e094f40640912ce6b46f6bf651f56
unaialberdi22 <ikbdr@plaiaundi.net>
2024-05-12 19:10:41 +0200

    Update 12/05/2024

--- Frontend/src/components/map.jsx
+++ Frontend/src/components/map.jsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 
 //imagenes
 import minus from '../images/minus.png';
-import getData from "../functions/getRutasByParadaId";
+import getRutas from "../functions/getRutasByParadaId";
 
 export default function Map() {
 const [paradas, setParadas] = useState([])
@@ -28,34 +28,39 @@ useEffect(() => {
 
 return(
     <div className="Mapa">
-    <MapContainer center={[40.463667, -3.74922]} zoom={6} scrollWheelZoom={true} style={{ height: "100%" }}>
-        <TileLayer
-            url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
-        />
-        {paradas.map((parada, index) =>{
-            return <Marker key={index} eventHandlers={{
-                click: (e) => {
-                  getData(parada.idParada, setRutaSeleccionada)
-                },
-              }} position={[parada.latitud, parada.longitud]} icon={new Icon({ iconUrl: markerIconPng, iconAnchor: [13, 10] })}>
-            <Popup>
-                <div style={{height: "50vh", width: "auto"}}>
-                    <div className="popupHeader">
-                        <h2>Estacion de tren <b>{parada.nombreParada}</b></h2>
-                    </div>
-                    <div className="insignias">
-                        {[...new Set(rutaSelecionada.map(ruta => ruta.tipo))].map((tipo, index) => {
-                                return<img title={tipo.toUpperCase()} src={"/src/images/" + tipo + ".png"}  width={20.2} height={24.4} alt="icono Minusvalidos" />
-                        })}
-                        <img title={(parseInt(parada.accesoMinus) === 1) ? 'Acceso para minusválidos' : ''} src={minus} style={{ visibility: (parseInt(parada.accesoMinus) === 2) ? 'hidden' : 'visible' }} width={24.4} height={24.4} alt="icono Minusvalidos" />
-                    </div>
-                    {/* <p>id estacion: {parada.idParada}</p> */}
-                </div>
-            </Popup>
+        <div className="seleccionador">
 
-            </Marker>
-        })}
-    </MapContainer>
+        </div>
+            <MapContainer className="mapaObjeto" center={[40.463667, -3.74922]} zoom={6} scrollWheelZoom={true}>
+                <TileLayer
+                    url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
+                />
+                {paradas.map((parada, index) =>{
+                    return <Marker key={index} eventHandlers={{
+                        click: (e) => {
+                        getRutas(parada.idParada, setRutaSeleccionada)
+                        },
+                    }} position={[parada.latitud, parada.longitud]} icon={new Icon({ iconUrl: markerIconPng, iconAnchor: [13, 10] })}>
+                    <Popup>
+                        <div className="divPopup"  style={{height: "50vh", width: "auto"}}>
+                            <div className="popupHeader">
+                                <h2>Estacion de tren <b>{parada.nombreParada}</b></h2>
+                                <p>Trenes disponibles</p>
+                            </div>
+                            <div className="insignias">
+                                {[...new Set(rutaSelecionada.map(ruta => ruta.tipo))].map((tipo, index) => {
+                                        return<img title={tipo.toUpperCase()} src={"/src/images/" + tipo + ".png"}  width={20.2} height={24.4} alt="icono Minusvalidos" />
+                                })}
+                                <img title={(parseInt(parada.accesoMinus) === 1) ? 'Acceso para minusválidos' : ''} src={minus} style={{ visibility: (parseInt(parada.accesoMinus) === 2) ? 'hidden' : 'visible' }} width={24.4} height={24.4} alt="icono Minusvalidos" />
+                            </div>
+                            <br></br>
+                            <button>Seleccionar estacion de salida</button>
+                        </div>
+                    </Popup>
+
+                    </Marker>
+                })}
+            </MapContainer>
 </div>
 );
 }
